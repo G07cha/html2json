5f8129a08d8c223b0ffb51ab70ab114468329ddc
G07cha <azizovkostya97@gmail.com>
2015-08-08 11:13:33 +0300

    commander integration
    Added cli support

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-var downloader = require('./lib/downloader');
-var commander = require('commander');
+#!/usr/bin/env node
 
-downloader.downloadPage('https://www.google.com.ua/');
+var downloader = require('./lib/downloader'),
+    converter  = require('./lib/converter'),
+    program    = require('commander');
+
+//Basic help
+program
+    .version('0.1.0')
+    .option('-c --console', 'Log result to console')
+    .option('-o --output', 'Set output file(output.json by default)')
+    .option('-v --verbose', 'Add verbose logging')
+    .command('parse [website]')
+    .description('Parse provided website')
+
+    //Main function
+    .action(function(website, options) {
+        var response = downloader.downloadPage(website);
+        var result = converter.convertToJSON(response);
+        if(options.console) {
+            console.log(result);
+        } 
+    });
+
+// Examples
+program.on('--help', function(){
+    console.log('  Examples:');
+    console.log('');
+    console.log('    html2json www.google.com');
+    console.log('    html2json -o google.json www.google.com');
+    console.log('');
+});
+
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
