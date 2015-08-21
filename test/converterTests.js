dd0ecbf34a216e2586b771ba907ba6576b05d949
G07cha <azizovkostya97@gmail.com>
2015-08-21 09:11:03 +0000

    Added pending tests to converterTests

--- test/converterTests.js
+++ test/converterTests.js
@@ -10,4 +10,10 @@ describe('Converter', function() {
     it('should return array of "a" objects', function() {
         assert.equal(JSON.stringify(converter.convertToJSON("<a>0</a><a>1</a>")), JSON.stringify({a: [ { text: "0" }, { text: "1" } ] }));
     });
+    
+    it('should return element with one-liner tag child and normal tag child');
+    
+    it('should return correct form');
+    
+    it('should return currect folding elements');
 });
