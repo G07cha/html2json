7b524db0fd214e59d0a130a899bc547810db0034
G07cha <azizovkostya97@gmail.com>
2015-08-27 16:43:44 +0000

    Added more converter tests
    Testing several and boolean attributes

--- test/converterTests.js
+++ test/converterTests.js
@@ -1,7 +1,6 @@
 var converter = require('../lib/converter');
 var assert = require('assert');
 
-
 describe('Converter', function() {
     it('should return object with head element containing text property', function () {
         assert.equal(JSON.stringify(converter.convertToJSON("<head>foo</head>")), JSON.stringify({head: { text: "foo"}}));
@@ -11,9 +10,18 @@ describe('Converter', function() {
         assert.equal(JSON.stringify(converter.convertToJSON("<a>0</a><a>1</a>")), JSON.stringify({a: [ { text: "0" }, { text: "1" } ] }));
     });
     
+    it('should return object with \'true\' value', function() {
+        assert.equal(JSON.stringify(converter.convertToJSON('<test hidden></test>')), JSON.stringify( { test: {hidden: true} } ));
+    });
+    
+    it('should return several attributes for tag', function() {
+        assert.equal(JSON.stringify(converter.convertToJSON('<test foo=bar hidden myname=skrillex>yo</test>')), JSON.stringify( { test: {foo: 'bar', hidden: true, myname: 'skrillex', text:'yo'}}));
+    });
+    it('should ignore comments');
+    
     it('should return element with one-liner tag child and normal tag child');
     
-    it('should return correct form');
+    it('should return form object');
     
     it('should return currect folding elements');
 });
