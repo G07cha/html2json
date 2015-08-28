c5541d13ae8dc75e82cb60b4d60d5077cf660648
G07cha <azizovkostya97@gmail.com>
2015-08-28 07:11:07 +0000

    Improved converter tests
    - Added tests for comments
    - Moved converting to separate functions so now tests looks

--- test/converterTests.js
+++ test/converterTests.js
@@ -1,27 +1,32 @@
 var converter = require('../lib/converter');
 var assert = require('assert');
 
+function compareObjects(html, expectingObject) {
+    assert.equal(JSON.stringify(converter.convertToJSON(html)), JSON.stringify(expectingObject));
+}
+
 describe('Converter', function() {
     it('should return object with head element containing text property', function () {
-        assert.equal(JSON.stringify(converter.convertToJSON("<head>foo</head>")), JSON.stringify({head: { text: "foo"}}));
+        compareObjects('<head>foo</head>', { head: { text: "foo" } });
     });
         
     it('should return array of "a" objects', function() {
-        assert.equal(JSON.stringify(converter.convertToJSON("<a>0</a><a>1</a>")), JSON.stringify({a: [ { text: "0" }, { text: "1" } ] }));
+        compareObjects("<a>0</a><a>1</a>", {a: [ { text: "0" }, { text: "1" } ] });
     });
     
     it('should return object with \'true\' value', function() {
-        assert.equal(JSON.stringify(converter.convertToJSON('<test hidden></test>')), JSON.stringify( { test: {hidden: true} } ));
+        compareObjects('<test hidden></test>', { test: { hidden: true } });
     });
     
     it('should return several attributes for tag', function() {
-        assert.equal(JSON.stringify(converter.convertToJSON('<test foo=bar hidden myname=skrillex>yo</test>')), JSON.stringify( { test: {foo: 'bar', hidden: true, myname: 'skrillex', text:'yo'}}));
+        compareObjects('<test foo=bar hidden myname=skrillex>yo</test>',  { test: { foo: 'bar', hidden: true, myname: 'skrillex', text:'yo' } } );
     });
-    it('should ignore comments');
+    
+    it('should ignore comments', function() {
+        compareObjects('<html><!--comment--></html><!--another comment-->', { html: { } });
+    });
     
     it('should return element with one-liner tag child and normal tag child');
     
-    it('should return form object');
-    
-    it('should return currect folding elements');
+    it('should return correct element folding');
 });
